0401249df76001b9b975cda7ceff350c4dad6787
Chad Humphries <chad@spicycode.com>
2010-09-09 17:36:19 -0400

    textmate-roots should have an easily identifiable home project folder to make everything friendly like

--- local.el
+++ local.el
@@ -6,7 +6,7 @@
  '("vendor" "spicycode" "vendor/language-modes"
    "vendor/language-modes/markdown" "vendor/language-modes/ruby"
    "vendor/language-modes/ruby/cucumber" "vendor/nav" "vendor/sunburst"
-   "vendor/tango" "vendor/textmate"))
+   "vendor/tango"))
 
 
 ; Please save my customizations here, and don't error if the file does not exist
